ba53c476ad8c8117356ca425db5d6105a4a1cef3
Sadid <sadid.jones@gmail.com>
2023-03-08 16:40:25 +0600

    @8/3/2023

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,28 @@
 import React from "react";
-
+import { Typography, Button, Container } from "@mui/material";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 function Create() {
-  return <div>Create page</div>;
+  return (
+    <Container>
+      <Typography
+        variant="h6"
+        component={`h2`}
+        gutterBottom
+        color={`textSecondary`}
+      >
+        Create a new Note
+      </Typography>
+      <Button
+        onClick={() => console.log("you clicked me")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}
+      >
+        Submit
+      </Button>
+    </Container>
+  );
 }
 
 export default Create;
