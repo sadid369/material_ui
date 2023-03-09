e4754dabb38973c1306291aff7ca13d98d6daab8
Sadid <sadid.jones@gmail.com>
2023-03-09 23:35:23 +0600

    @9/03/2023

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Notes from "./pages/Notes";
 import Create from "./pages/Create";
-
+import { createTheme, ThemeProvider } from "@mui/material";
+import { purple } from "@mui/material/colors";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#fefefe",
+    },
+    secondary: purple,
+  },
+  typography: {
+    fontFamily: "Quicksand",
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Notes />}></Route>
-        <Route path="/create" element={<Create />}></Route>
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Notes />}></Route>
+          <Route path="/create" element={<Create />}></Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,14 +2,18 @@ import React from "react";
 import { Typography, Button, Container } from "@mui/material";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import { makeStyles } from "@mui/styles";
-
+// import { styled } from "@mui/system";
 const useStyles = makeStyles({
   btn: {
-    backgroundColor: "red",
-    fontSize: 100,
+    backgroundColor: "violet",
+    fontSize: 6,
+    "&:hover": {
+      backgroundColor: "red",
+    },
   },
   title: {
-    fontSize: 600,
+    textDecoration: "underline",
+    marginBottom: 20,
   },
 });
 
@@ -18,16 +22,17 @@ function Create() {
   return (
     <Container>
       <Typography
-        className={classes.title}
         variant="h6"
         component={`h2`}
         gutterBottom
         color={`textSecondary`}
+        className={classes.title}
       >
         Create a new Note
       </Typography>
       <Button
-        className={classes.btn}
+        // className={classes.btn}
+        color="secondary"
         onClick={() => console.log("you clicked me")}
         type="submit"
         variant="contained"
